e3697e931425dc2343b252c99635295a5dd65737
Thami Jasop <jasopthami@gmail.com>
2026-05-21 16:43:13 +0200

    Fix production API URL

--- client/src/hooks/useFetch.js
+++ client/src/hooks/useFetch.js
@@ -1,38 +1,50 @@
 import { useEffect, useState } from "react";
 import axios from "axios";
 
-const BASE_URL = "/api";
+const BASE_URL = "https://luxstay-rlva.onrender.com/api";
 
 const useFetch = (url) => {
   const [data, setData] = useState([]);
-  const [loading, setLoading] = useState(false);
+  const [loading, setLoading] = useState(true);
   const [error, setError] = useState(false);
 
   useEffect(() => {
     const fetchData = async () => {
       setLoading(true);
+
       try {
-        const res = await axios.get(BASE_URL + url);
+        const res = await axios.get(BASE_URL + url, {
+          withCredentials: true,
+        });
+
         setData(res.data);
-      } catch (e) {
-        setError(e);
-        console.log(e); // helps debugging
+      } catch (err) {
+        console.log(err);
+
+        setError(true);
       }
+
       setLoading(false);
     };
 
     fetchData();
-  }, [url]); // 👈 important (refetch when URL changes)
+  }, [url]);
 
   const reFetchData = async () => {
     setLoading(true);
+
     try {
-      const res = await axios.get(BASE_URL + url);
+      const res = await axios.get(BASE_URL + url, {
+        withCredentials: true,
+      });
+
       setData(res.data);
-    } catch (e) {
-      setError(e);
-      console.log(e);
+    } catch (err) {
+      console.log(err);
+
+      setError(true);
     }
+
     setLoading(false);
   };
 
